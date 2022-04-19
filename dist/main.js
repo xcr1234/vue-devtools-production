8563733862b2f07fce264c980fee5bd2f6b454d1
xcr1234 <530551426@qq.com>
2022-04-20 00:39:51 +0800

    更新一下header

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Vue生产环境（production） Devtools 调试
-// @namespace       https://github.com/xcr1234/vue-production-devtools
+// @namespace       https://github.com/xcr1234/vue-devtools-production
 // @version         1.0.0
 // @description     使用本脚本支持直接调试生产环境的Vue项目 支持Vue2、Vue3！
 // @include         *
--- header/index.ts
+++ header/index.ts
@@ -3,7 +3,7 @@ import build from "./build";
 
 const script: UserScript = {
     name: 'Vue生产环境（production） Devtools 调试',
-    namespace: 'https://github.com/xcr1234/vue-production-devtools',
+    namespace: 'https://github.com/xcr1234/vue-devtools-production',
     description: '使用本脚本支持直接调试生产环境的Vue项目 支持Vue2、Vue3！',
     version: '1.0.0',
     includes: [
